7429d96b3a5ed20c7d0dd8ccbf61cc523bcd15a3
chen <toaff100@gmail.com>
2024-04-28 15:35:18 +0300

    refactor: ProjectsModel and TasksModel

--- backend/src/models/ProjectsModel.ts
+++ backend/src/models/ProjectsModel.ts
@@ -3,10 +3,19 @@ import IProject from "../interfaces/IProject";
 
 const ProjectSchema: Schema = new Schema({
   name: { type: String, required: true },
-  description: { type: String, required: true },
+  description: { type: String },
   tasks: [{ type: Schema.Types.ObjectId, ref: "Task" }],
-  manager: { type: Schema.Types.ObjectId, ref: "Person", required: true },
-  teamMembers: [{ type: Schema.Types.ObjectId, ref: "Person" }],
+  creator: { type: Schema.Types.ObjectId, ref: "Person", required: true },
+  members: [{ type: Schema.Types.ObjectId, ref: "Person" }],
 });
+
+// Add a virtual property 'id' that's derived from '_id'.
+ProjectSchema.virtual("id").get(function (this: IProject) {
+  return this._id.toHexString();
+});
+
+// Ensure virtuals are included when converting a document to JSON/objects
+ProjectSchema.set("toJSON", { virtuals: true });
+ProjectSchema.set("toObject", { virtuals: true });
 
 export default mongoose.model<IProject>("Project", ProjectSchema);
--- backend/src/models/TasksModel.ts
+++ backend/src/models/TasksModel.ts
@@ -1,7 +1,8 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import ITask from "../interfaces/ITask";
 
 const TaskSchema: Schema = new Schema({
+  _id: { type: Schema.Types.ObjectId, alias: "id" }, // Alias _id to id
   title: { type: String, required: true },
   description: { type: String, required: true },
   status: {
@@ -12,5 +13,14 @@ const TaskSchema: Schema = new Schema({
   assignedTo: { type: Schema.Types.ObjectId, ref: "Person" },
   dueDate: { type: Date },
 });
+
+// Add a virtual property 'id' that's derived from '_id'.
+TaskSchema.virtual("id").get(function (this: ITask) {
+  return this._id.toHexString();
+});
+
+// Ensure virtuals are included when converting a document to JSON/objects
+TaskSchema.set("toJSON", { virtuals: true });
+TaskSchema.set("toObject", { virtuals: true });
 
 export default mongoose.model<ITask>("Task", TaskSchema);
