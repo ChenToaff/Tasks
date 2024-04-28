d61f7b74380ccb28c8e15e780d51e3b7200f58ef
chen <toaff100@gmail.com>
2024-04-28 15:32:16 +0300

    refactored IProject

--- backend/src/interfaces/IProject.ts
+++ backend/src/interfaces/IProject.ts
@@ -4,6 +4,5 @@ export default interface IProject extends Document {
   name: string;
   description: string;
   tasks: Schema.Types.ObjectId[]; // Array of task references
-  manager: Schema.Types.ObjectId; // Link to a Person document who manages the project
-  teamMembers: Schema.Types.ObjectId[]; // Array of Person document references
+  members: Schema.Types.ObjectId[]; // Array of Person document references
 }
