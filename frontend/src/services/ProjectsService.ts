b5d008b59fb45ed817450b97663ceecafef8259f
chen <toaff100@gmail.com>
2024-05-04 18:43:58 +0300

    refacotr: ProjectsService

--- frontend/src/services/ProjectsService.ts
+++ frontend/src/services/ProjectsService.ts
@@ -4,11 +4,33 @@ import axios from "axios";
 const API_URL = "/api/projects";
 
 class ProjectsService {
+  async getAllUserProjects(): Promise<IProject[] | null> {
+    try {
+      const response = await axios.get(API_URL);
+      return response.data.projects;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+
   async createProject(
     newProjectData: Partial<IProject>
   ): Promise<IProject | null> {
     try {
       const response = await axios.post(API_URL, newProjectData);
+      return response.data;
+    } catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+  async createTaskColumn(projectId: string, title: string) {
+    try {
+      const response = await axios.post(`${API_URL}/add-column`, {
+        projectId,
+        title,
+      });
       return response.data;
     } catch (error) {
       console.log(error);
