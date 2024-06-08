a6521b91635c538558fd2033cfd58cdb78b84d50
chen <toaff100@gmail.com>
2024-06-08 19:50:32 +0300

    feat: add colleaguesService

---
+++ backend/src/services/colleaguesService.ts
@@ -0,0 +1,47 @@
+import IPerson from "../interfaces/IPerson";
+import PersonModel from "../models/PeopleModel";
+import bcrypt from "bcryptjs";
+
+export const getColleagues = async (
+  userId: string
+): Promise<Partial<IPerson>[] | null> => {
+  const user = await PersonModel.findById<IPerson>(userId, "people")
+    .populate({
+      path: "colleagues",
+      select: "username name",
+    })
+    .exec();
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const colleagues = user.colleagues as IPerson[];
+  return colleagues;
+};
+
+// export const addColleague = async (
+//   id: string,
+//   data: UpdatePersonData
+// ): Promise<IPerson | null> => {
+//   if (data.password) {
+//     data.password = await bcrypt.hash(data.password, 10);
+//   }
+//   const updateData = {
+//     ...data,
+//     passwordHash: data.password,
+//   };
+//   delete updateData.password; // Remove plaintext password if exists
+
+//   const updatedPerson = await PersonModel.findByIdAndUpdate(id, updateData, {
+//     new: true,
+//     select: "-_id -__v",
+//   }).exec();
+//   return updatedPerson;
+// };
+
+// export const removeColleague = async (id: string): Promise<IPerson | null> => {
+//   const deletedPerson = await PersonModel.findByIdAndDelete(id).select(
+//     "-_id -__v"
+//   );
+//   return deletedPerson;
+// };
