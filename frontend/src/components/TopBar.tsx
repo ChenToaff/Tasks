123c303dbdedb35163c197ad57b4248bef08f402
chen <toaff100@gmail.com>
2024-06-12 18:25:12 +0300

    refactor(frontend): remove unnecessary code from TopBar

--- frontend/src/components/TopBar.tsx
+++ frontend/src/components/TopBar.tsx
@@ -5,17 +5,17 @@ import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
 import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
+import TaskAltIcon from "@mui/icons-material/TaskAlt";
 import { useAuth } from "@hooks/useAuth";
-import { useUser } from "@hooks/useUser";
+import { useUser } from "@features/user/hooks/useUser";
 
-const settings = ["Profile", "Logout"];
+import { Logout, Person } from "@mui/icons-material";
+import { ListItemIcon } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function TopBar() {
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
@@ -46,84 +46,69 @@ function TopBar() {
       position="fixed"
       sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
     >
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/home"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontWeight: 700,
-              letterSpacing: ".15rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            Tasks
-          </Typography>
+      <Toolbar>
+        <TaskAltIcon sx={{ mr: 1 }} />
+        <Typography
+          variant="h6"
+          noWrap
+          component="a"
+          href="/home"
+          sx={{
+            mr: 2,
+            fontWeight: 700,
+            letterSpacing: ".15rem",
+            color: "inherit",
+            textDecoration: "none",
+          }}
+        >
+          Tasks
+        </Typography>
 
-          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}></Box>
+        <Box sx={{ flexGrow: 1, display: "flex" }}></Box>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar>
-                  {user?.name
-                    .split(" ")
-                    .map((n) => n[0])
-                    .join(" ")}
-                </Avatar>
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              <MenuItem onClick={logout}>
-                <Typography textAlign="center">logout</Typography>
-              </MenuItem>
-              <MenuItem onClick={handleCloseUserMenu}>
-                <Typography textAlign="center">profile</Typography>
-              </MenuItem>
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
+        <Box sx={{ flexGrow: 0 }}>
+          <Tooltip title="Open settings">
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar>
+                {user?.name
+                  .split(" ")
+                  .map((n) => n[0])
+                  .join(" ")}
+              </Avatar>
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: "45px" }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            <MenuItem onClick={logout}>
+              <ListItemIcon>
+                <Logout />
+              </ListItemIcon>
+
+              <Typography textAlign="center">Logout</Typography>
+            </MenuItem>
+            <MenuItem onClick={handleCloseUserMenu}>
+              <ListItemIcon>
+                <Person />
+              </ListItemIcon>
+              <Typography textAlign="center">Profile</Typography>
+            </MenuItem>
+          </Menu>
+        </Box>
+      </Toolbar>
     </AppBar>
   );
 }
