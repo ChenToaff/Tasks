98b15d6b0de91e0cbd5b4749f7b01e585afe456d
chen <toaff100@gmail.com>
2024-06-12 17:06:11 +0300

    refactor(backend): add id to ItaskColumn

--- backend/src/interfaces/ITaskColumn.ts
+++ backend/src/interfaces/ITaskColumn.ts
@@ -1,6 +1,7 @@
 import { Schema, Document, Types } from "mongoose";
 
 export default interface ITaskColumn extends Document {
+  id: string;
   title: string;
-  tasks: Types.ObjectId[]; // Array of task IDs
+  tasks: Types.ObjectId[];
 }
