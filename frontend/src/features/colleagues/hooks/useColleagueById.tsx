d71fa22d641f2ce5a57dd4fc9f0678e50e7c64b5
chen <toaff100@gmail.com>
2024-06-12 21:41:06 +0300

    fix(frontend): correct type of useColleagueById hook

--- frontend/src/features/colleagues/hooks/useColleagueById.tsx
+++ frontend/src/features/colleagues/hooks/useColleagueById.tsx
@@ -2,9 +2,9 @@ import { useSelector } from "react-redux";
 import { RootState, AppDispatch } from "../../../store";
 import IPerson from "@interfaces/IPerson";
 
-const useColleagueById = (id?: string): IPerson | undefined => {
+const useColleagueById = (id?: string): IPerson | null => {
   return useSelector((state: RootState) =>
-    id ? state.colleagues.data[id] : undefined
+    id ? state.colleagues.data[id] : null
   );
 };
 
