59360b2ff6e4c7b44e99da9b280a9ac494adb3ac
chen <toaff100@gmail.com>
2024-06-12 21:46:55 +0300

    feat(frontend): add the user to AssignButton selection and add usage of useUpdateEffect

--- frontend/src/features/tasks/components/AssignButton.tsx
+++ frontend/src/features/tasks/components/AssignButton.tsx
@@ -16,30 +16,44 @@ import ITask from "@interfaces/ITask";
 import TasksService from "../api/TasksService";
 import Avatar from "@mui/material/Avatar";
 import useColleagueById from "@features/colleagues/hooks/useColleagueById";
+import { useUser } from "@features/user/hooks/useUser";
+import useUpdateEffect from "@hooks/useUpdateEffect";
 
 export default function AssignButton({ task }: { task: ITask }) {
   const { colleagues } = useColleagues();
+  const { user } = useUser();
+  const options = [
+    { name: "You", username: user?.username, id: user?.id },
+    ...Object.values(colleagues),
+  ];
   const colleague = useColleagueById(task.assignedTo);
+  const assignee = task.assignedTo == user?.id ? user : colleague;
+  const [selectedUser, setSelectedUser] = useState<Partial<IPerson> | null>(
+    assignee
+  );
+
   const [open, setOpen] = useState(false);
   const buttonRef = useRef(null);
-  const inputRef = useRef(null);
+  const inputRef = useRef<HTMLInputElement>(null);
   const filterOptions = createFilterOptions({
     stringify: ({ name, username, id }: Partial<IPerson>) =>
       `${name} ${username} ${id}`,
   });
 
-  function assignToUser(e: any, selectedUser: Partial<IPerson> | null) {
-    TasksService.assignTask(task.id, selectedUser?.id ?? null);
-    setOpen(false);
-  }
+  useUpdateEffect(() => {
+    function assignToUser() {
+      TasksService.assignTask(task.id, selectedUser?.id ?? null);
+    }
+    assignToUser();
+  }, [selectedUser]);
 
   return (
     <div onClick={(e) => e.stopPropagation()}>
       <Tooltip title="Assign this task">
         <IconButton ref={buttonRef} onClick={() => setOpen(true)}>
-          {colleague ? (
+          {assignee ? (
             <Avatar sx={{ width: "24px", height: "24px", fontSize: "small" }}>
-              {colleague.name
+              {assignee.name
                 .split(" ")
                 .map((n) => n[0])
                 .join(" ")}
@@ -67,11 +81,12 @@ export default function AssignButton({ task }: { task: ITask }) {
           <Autocomplete
             openOnFocus
             id="tags-outlined"
-            defaultValue={colleague}
-            options={Object.values(colleagues)}
-            onChange={assignToUser}
+            value={selectedUser}
+            options={options}
+            onChange={(e, value) => setSelectedUser(value)}
             // defaultValue={{ username: task.assignedTo }}
             filterOptions={filterOptions}
+            isOptionEqualToValue={(option, value) => option.id === value.id}
             getOptionLabel={({ username }) => {
               return `${username}`;
             }}
