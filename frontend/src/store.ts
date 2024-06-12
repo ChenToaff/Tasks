e9a6a15c40d4ea83197cdc391c0cfc98f02aab74
chen <toaff100@gmail.com>
2024-06-12 18:09:50 +0300

    refactor(frontend): update store.ts

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,13 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-import projectsReducer from "./features/projects/redux/projectsReducer.ts";
-import tasksReducer from "@features/tasks/redux/tasksReducer.ts";
+import projectsReducer from "./features/projects/redux/projectsSlice.ts";
+import tasksReducer from "@features/tasks/redux/tasksSlice.ts";
+import colleaguesReducer from "@features/colleagues/redux/colleaguesSlice.ts";
 
 const store = configureStore({
   reducer: {
     projects: projectsReducer,
     // people: peopleReducer,
     tasks: tasksReducer,
+    colleagues: colleaguesReducer,
   },
+  devTools: true,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
