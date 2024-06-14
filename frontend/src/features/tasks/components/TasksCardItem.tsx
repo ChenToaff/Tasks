b09f38b2ec796caaf9e96bdd20b3f942c9f93de6
chen <toaff100@gmail.com>
2024-06-14 21:37:48 +0300

    feat(frontend): add project chip to TaskCardItem

--- frontend/src/features/tasks/components/TasksCardItem.tsx
+++ frontend/src/features/tasks/components/TasksCardItem.tsx
@@ -1,18 +1,40 @@
 import ITask from "@interfaces/ITask";
-import { ListItem, ListItemButton } from "@mui/material";
+import { Box, Chip, ListItem, ListItemButton, Tooltip } from "@mui/material";
 import { useSelectedTask } from "../hooks/useSelectedTask";
+import useSelectedProject from "@features/projects/hooks/useSelectedProject";
+import { useNavigate } from "react-router-dom";
 
 export default function TasksCardItem({ task }: { task: ITask }) {
   const { setSelectedTask } = useSelectedTask();
+  const { project } = useSelectedProject(task.projectId);
+  const navigate = useNavigate();
   return (
     <ListItem
       dense
       divider
       disablePadding
+      alignItems="flex-start"
       onClick={() => setSelectedTask(task)}
     >
       <ListItemButton>
         <p>{task.title}</p>
+        <Box margin="auto" />
+        {project && (
+          <Tooltip
+            arrow
+            placement="top"
+            title={`Click to view all tasks in ${project.name}`}
+          >
+            <Chip
+              onClick={() => {
+                navigate(`/projects/${project.id}`);
+              }}
+              variant="outlined"
+              label={project.name}
+              size="small"
+            />
+          </Tooltip>
+        )}
       </ListItemButton>
     </ListItem>
   );
