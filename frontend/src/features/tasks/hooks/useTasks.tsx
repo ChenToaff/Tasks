bd5261525a487ca7da92cfcbeeb3fa32b4c869f1
chen <toaff100@gmail.com>
2024-06-14 16:47:28 +0300

    feat(frontend): useTasks now loads the initial tasks

--- frontend/src/features/tasks/hooks/useTasks.tsx
+++ frontend/src/features/tasks/hooks/useTasks.tsx
@@ -1,14 +1,21 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 
-import { RootState } from "../../../store";
+import { RootState, AppDispatch } from "../../../store";
 
 import { selectTasksByAssignee } from "@features/tasks/redux/tasksSelectors";
-
+import { loadInitialTasks } from "../redux/tasksActions";
 // Custom hook to use a selected project and ensure its full data is loaded
 const useTasks = (assignee: string) => {
+  const dispatch = useDispatch<AppDispatch>();
   const tasks = useSelector((state: RootState) =>
     selectTasksByAssignee(state, assignee)
   );
+
+  // Initially load tasks
+  useEffect(() => {
+    dispatch(loadInitialTasks());
+  }, []);
   return {
     tasks,
   };
