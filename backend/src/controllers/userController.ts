2ae91b10fd86684dc70514c8cdb382fc15588ccf
chen <toaff100@gmail.com>
2024-06-16 23:06:57 +0300

    feat(backend): add validation to createUser route

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import asyncHandler from "../utils/asyncHandler";
 import * as UserService from "../services/userService";
+import { ValidationError } from "../utils/ApiError";
 
 export const getAllUsers = asyncHandler(async (req: Request, res: Response) => {
   const users = await UserService.findAllUsers();
@@ -15,6 +16,14 @@ export const getUser = asyncHandler(async (req: Request, res: Response) => {
 });
 
 export const createUser = asyncHandler(async (req: Request, res: Response) => {
+  const { username, password, name } = req.body;
+  if (!username || !password || !name) {
+    throw new ValidationError(
+      "The request is missing one or more required fields: username, password, name."
+    );
+  }
+  const userExists = await UserService.findUserByUsername(username);
+  if (userExists) throw new ValidationError("Username already taken.");
   const newUser = await UserService.createUser(req.body);
   res
     .status(201)
