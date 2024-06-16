851a476fb28642b7092e48ede4a95fde9987472d
chen <toaff100@gmail.com>
2024-06-16 22:28:43 +0300

    feat(frontend): add custom axiosInstance

--- frontend/src/lib/axiosInstance.ts
+++ frontend/src/lib/axiosInstance.ts
@@ -1,36 +1,40 @@
-// import {
-//   AxiosError,
-//   AxiosInstance,
-//   AxiosRequestConfig,
-//   AxiosResponse,
-// } from "axios";
-// import axios from "axios";
+import {
+  AxiosError,
+  AxiosInstance,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from "axios";
+import axios from "axios";
 
-// const onRequest = (config: AxiosRequestConfig): AxiosRequestConfig => {
-//   console.info(`[request] [${JSON.stringify(config)}]`);
-//   return config;
-// };
+const onRequest = (
+  config: InternalAxiosRequestConfig
+): InternalAxiosRequestConfig => {
+  return config;
+};
 
-// const onRequestError = (error: AxiosError): Promise<AxiosError> => {
-//   console.error(`[request error] [${JSON.stringify(error)}]`);
-//   return Promise.reject(error);
-// };
+const onRequestError = (error: AxiosError): Promise<AxiosError> => {
+  return Promise.reject(error);
+};
 
-// const onResponse = (response: AxiosResponse): AxiosResponse => {
-//   console.info(`[response] [${JSON.stringify(response)}]`);
-//   return response;
-// };
+const onResponse = (response: AxiosResponse): AxiosResponse => {
+  return response;
+};
 
-// const onResponseError = (error: AxiosError): Promise<AxiosError> => {
-//   console.error(`[response error] [${JSON.stringify(error)}]`);
-//   return Promise.reject(error);
-// };
+const onResponseError = (error: AxiosError): Promise<AxiosError> => {
+  if (error.response && error.response.status === 400) {
+    const errorMessage =
+      (error.response.data as { message: string }).message ||
+      "An error occurred";
+    return Promise.reject(new Error(errorMessage));
+  }
+  return Promise.reject(error);
+};
 
-// function setupInterceptorsTo(axiosInstance: AxiosInstance): AxiosInstance {
-//   axiosInstance.interceptors.request.use(onRequest, onRequestError);
-//   axiosInstance.interceptors.response.use(onResponse, onResponseError);
-//   return axiosInstance;
-// }
+function setupInterceptorsTo(axiosInstance: AxiosInstance): AxiosInstance {
+  axiosInstance.interceptors.request.use(onRequest, onRequestError);
+  axiosInstance.interceptors.response.use(onResponse, onResponseError);
+  return axiosInstance;
+}
 
-// const axiosInstance = setupInterceptorsTo(axios.createInstance());
-// export default axiosInstance;
+const axiosInstance = setupInterceptorsTo(axios.create());
+export default axiosInstance;
